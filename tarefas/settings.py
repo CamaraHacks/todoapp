c798d425f5d193cf87cb4ddd66ee277d6fb31ad9
camarahacks <gustavo-camara@outlook.com>
2025-11-02 21:37:47 -0300

    debug false and allowedhosts all

--- tarefas/settings.py
+++ tarefas/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-98-owm%5q)lm%tc4797p!(3u=gb_recd3nmt$f=93l%vyk@p=1"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
