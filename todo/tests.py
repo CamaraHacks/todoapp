bc90a16c3d60688f6fbe028eef4e45e183d569ae
camarahacks <gustavo-camara@outlook.com>
2026-03-03 11:34:40 -0300

    teste task_related_to_user

--- todo/tests.py
+++ todo/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from .models import Task
 
-# Create your tests here.
+class TaskModelTest(TestCase):
+    def teste_task_belongs_to_user(self):
+        user = User.objects.create_user(username ='usuario_mocado', password='senhamocada')
+        task = Task.objects.create(title='task_mocada', user = user)
+        self.assertEqual(task.user, user)  
